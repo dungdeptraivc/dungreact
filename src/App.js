522756b0832b25606607dc92e3fda71dc043c28e
doandung <nguyendoandung123456789@gmail.com>
2025-07-31 15:01:27 +0700

    31/7

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Todolist from './component/Todolist';
+
+function App() {
+  return <Todolist/>;
+}
+
+export default App;
---
+++ src/component/Todolist.js
@@ -0,0 +1,8 @@
+import React from 'react'
+ export default function Todolist() {
+   return (
+  
+      <h1>Todo List</h1>
+    
+   )
+ }
